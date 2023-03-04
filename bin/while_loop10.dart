c7ddf3c5cdd5f4d27e388b525ed8544f9dd59b11
network7277 <husanjurayev7277@gmail.com>
2023-03-04 11:57:01 +0500

    10

--- bin/while_loop10.dart
+++ bin/while_loop10.dart
@@ -2,8 +2,24 @@
   Create function func with "a " argument
   If the number "a" is given, "tub son" is returned if it is a prime number,
   otherwise "tub son emas"
-  
-  
-  
-*/
-void main() {}
+  */
+
+String func( int a ){
+  String s1 = ' ';
+  String s2 = ' ';
+  int i = 2;
+  while ( i < a ){
+    if ( a % i == 0 ){
+      s1 = ' tub son emas ';
+      return s1;
+    }
+    else{
+      s2 = ' tub son ';
+    }
+    i ++;
+  }
+  return s2;
+}
+void main() {
+  print(func(8));
+}
