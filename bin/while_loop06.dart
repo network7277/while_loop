09cb33677b634da149776b64bdaf06af382c91ff
network7277 <husanjurayev7277@gmail.com>
2023-03-04 12:38:41 +0500

    06

--- bin/while_loop06.dart
+++ bin/while_loop06.dart
@@ -1,7 +1,6 @@
 int func( int a ){
   int s = 0;
   if ( a % 2 == 1 ){
-   
     int i = 0;
     while ( i <= a ){
       if ( i % 2 == 1 ){
@@ -10,7 +9,6 @@ int func( int a ){
       i ++;
     }
   }else{
-
     int i = 0;
     while ( i <= a ){
       if ( i % 2 == 0 ){
