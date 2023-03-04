a01bf1619b1802a74f7e187efdc9ed1b57319665
network7277 <husanjurayev7277@gmail.com>
2023-03-04 11:36:47 +0500

    09

--- bin/while_loop09.dart
+++ bin/while_loop09.dart
@@ -4,4 +4,17 @@
   
   
 */
-void main() {}
+String func( int a ){
+  String h = ' ';
+  int i = 0;
+  while ( i <= a ){
+    if ( i % 2 == 0 ){
+      h += i.toString();
+    }
+    i ++;
+  }
+  return h;
+}
+void main() {
+  print(func(10));
+}
